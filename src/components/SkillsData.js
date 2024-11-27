f75e156e2140ba95f568d68b2b0b4177c61e4418
AJAHN1999 <103020052+AJAHN1999@users.noreply.github.com>
2024-11-27 13:08:52 +1100

    first draft

---
+++ src/components/SkillsData.js
@@ -0,0 +1,24 @@
+const SkillsData = [
+  {
+    id: "1",
+    name: "Communication",
+    image: require("../assets/Images/communication.png"),
+  },
+  {
+    id: "2",
+    name: "Networking",
+    image: require("../assets/Images/networking.png"),
+  },
+  {
+    id: "3",
+    name: "Leadership",
+    image: require("../assets/Images/leadership.png"),
+  },
+  {
+    id: "4",
+    name: "Relationship",
+    image: require("../assets/Images/relationship.png"),
+  },
+];
+
+export default SkillsData;
